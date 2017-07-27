116a8baf1d297e46f096d2b9930cb9d8c2f40c6a
Miho Tse <mihotse@gmail.com>
2017-07-27 15:47:42 +0100

    Implement listing restaurants on the index page

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Byron')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('Byron')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
 end
