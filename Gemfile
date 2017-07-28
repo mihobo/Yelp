7b174b0c56e5d6c2fb38eb86a583df0674be373b
Miho Tse <mihotse@gmail.com>
2017-07-28 11:05:00 +0100

    Implement restaurants descriptions viewed on the page

--- Gemfile
+++ Gemfile
@@ -53,4 +53,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :test do
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'database_cleaner'
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -20,4 +20,45 @@ feature 'restaurants' do
       expect(page).not_to have_content('No restaurants yet')
     end
   end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Byron'
+      fill_in 'Description', with: 'Amazing Burgers and Cream Soda'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'Byron'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'viewing restaurants' do
+
+    let!(:byron){ Restaurant.create(name:'Byron', description: 'Amazing Burgers and Cream Soda') }
+
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'Byron'
+      expect(page).to have_content 'Byron'
+      expect(page).to have_content 'Amazing Burgers and Cream Soda'
+      expect(current_path).to eq "/restaurants/#{byron.id}"
+    end
+  end
+
+  context 'editing restaurants' do
+
+    before { Restaurant.create name: 'Byron', description: 'Amazing Burgers and Cream Soda', id: 1 }
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit Byron'
+      fill_in 'Name', with: 'Byron'
+      fill_in 'Description', with: 'Best burgers in town'
+      click_button 'Update Restaurant'
+      click_link 'Byron'
+      expect(page).to have_content 'Byron'
+      expect(page).to have_content 'Best burgers in town'
+      expect(current_path).to eq '/restaurants/1'
+    end
+  end
 end
